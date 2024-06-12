65ea4a5a2c2bd01148f6529d3881584dccc3fcca
Mirko Abozzi <mirko.abozzi@gmail.com>
2024-06-12 15:55:30 +0200

    libreria

---
+++ index.js
@@ -0,0 +1,54 @@
+const fetchBooks = () => {
+  fetch("https://striveschool-api.herokuapp.com/books")
+    .then((booksApiObj) => {
+      if (booksApiObj.ok) {
+        console.log(booksApiObj);
+        return booksApiObj.json();
+      } else {
+        throw Error("Dati non ricevuti");
+      }
+    })
+    .then((booksObj) => {
+      console.log(booksObj);
+
+      const booksContainer = document.getElementById("booksContainer");
+
+      booksObj.forEach((book) => {
+        const col = document.createElement("div");
+        col.classList.add("col");
+        const card = document.createElement("div");
+        card.classList.add("card", "g-5");
+        const img = document.createElement("img");
+        img.classList.add("card-img-top", "h-100");
+        img.setAttribute("src", `${book.img}`);
+        const title = document.createElement("h5");
+        title.classList.add("card-title");
+        title.innerText = `${book.title}`;
+        const price = document.createElement("p");
+        price.innerText = `€ ${book.price}`;
+        const btn = document.createElement("a");
+        btn.classList.add("btn", "btn-primary", "mb-4");
+        btn.innerText = "Remove book";
+        const addToCart = document.createElement("a");
+        addToCart.classList.add("btn", "btn-primary", "mb-4");
+        addToCart.innerText = "Add To Cart";
+
+        card.appendChild(img);
+        card.appendChild(title);
+        card.appendChild(price);
+        card.appendChild(btn);
+        card.appendChild(addToCart);
+        col.appendChild(card);
+        booksContainer.appendChild(card);
+
+        btn.addEventListener("click", (event) => {
+          card.remove();
+        });
+      });
+    })
+    .catch((error) => console.log(error));
+};
+
+window.addEventListener("DOMContentLoaded", () => {
+  fetchBooks();
+});
